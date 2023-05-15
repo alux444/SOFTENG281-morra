4f6c411ca6018bd6e5be76033fd833a8c7858aa2
Alex Liang <alex@Alexs-MacBook-Air.local>
2023-05-16 00:45:27 +1200

    completed task 1

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -4,11 +4,18 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Morra {
 
+  MorraGame currentGame;
+
   public Morra() {}
 
-  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {}
+  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
+    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    this.currentGame = new MorraGame(difficulty, pointsToWin, options[0]);
+  }
 
-  public void play() {}
+  public void play() {
+    currentGame.playRound();
+  }
 
   public void showStats() {}
 }
